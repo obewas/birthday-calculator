c136b4443c77e2cbcb1d2872950e079a8b28b98d
obewas <obewas1202@gmail.com>
2020-07-21 09:35:09 +0300

    changed function call

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
-  function dayOfWeek(){
   
-  let year = document.getElementById(".year").value;
-  let month = document.getElementById(".month").value;
-  let date = document.getElementById(".date").value;
-  let gender = document.getElementById(".gender").value;
+function dayOfWeek() {
   
-  
-  if (month > 12 || date > 31) {
-    document.getElementById("p").innerHTML = "You entered and invalid number, try again"
-  } else {
-  var result = year + month+ date
-  }
-  dayOfBirth = result
-    
-  CC = parseInt(dayOfBirth.slice(0,2))
-  YY = parseInt(dayOfBirth.slice(2,4))
-  MM = parseInt(dayOfBirth.slice(4,6))
-  DD = parseInt(dayOfBirth.slice(6,8))
-  
-  
-  d = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-  d = Math.floor(d)
-  alert(d)
-  nameOfDays = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","saturday"];
-  alert("Your were born on a " + nameOfDays[d]);
-  male = ['Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw',' Kofi', 'Kwame'];
-  female = ['Akosua', 'Adwoa', 'Abenaa','Akua', 'Yaa','Afua','Ama'];
-  if (MM > 12 || DD > 31) {
-    document.getElementById("p").innerHTML = "You entered and invalid number, try again";
-  } 
-  else if (gender === 'male') {
-    document.getElementById("p").innerHTML = "Your name is " + male[d];
-  } else {
-    document.getElementById("p").innerHTML = 'Your name is '+ female[d];
-  }
+
+var year = document.getElementById("year").value;
+var month = document.getElementById("month").value;
+var date = document.getElementById("date").value;
+
+var fullDate = year + month + date;
 }
-document.getElementById(.btn).addEventListener("click",dayOfWeek);
+var dayOfBirth = fullDate
+//document.write(dayOfBirth)
+   
+var CC = parseInt(dayOfBirth.slice(0,2))
+var YY = parseInt(dayOfBirth.slice(2,4))
+var MM = parseInt(dayOfBirth.slice(4,6))
+var DD = parseInt(dayOfBirth.slice(6,8))
+  
+ var a = Math.floor(CC/4 - 2*CC-1)
+ var b = Math.floor(5*YY/4)
+ var c = Math.floor(26*(MM+1)/10)
+ var d = DD
+ var weekday = (a + b + c + d)
+ weekday = weekday % 7
+ 
+
+ var nameOfDays = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","saturday"];
+ var male = ['Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw',' Kofi', 'Kwame'];
+ var female = ['Akosua', 'Adwoa', 'Abenaa','Akua', 'Yaa','Afua','Ama'];
+ document.write("Your were born on a " + nameOfDays[weekday]);
+
+var weekday = 1
+var gender = document.getElementById("gender").value;
+  if (gender === 'male') {
+    document.getElementById('result')= "Your name is " + male[weekday];
+  } else 
+   document.getElementById('result')='Your name is '+ female[weekday];
